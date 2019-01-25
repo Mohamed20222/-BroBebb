9dd112fcab0bd4d1d15edb2a2aceef543903733a
Mohamed20222 <44948619+Mohamed20222@users.noreply.github.com>
2019-01-25 12:01:55 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`=bc |VIRUS4EVER`,"http://twitch.tv/Death Shop")
+client.user.setGame(`VIRUS4EVER`,"http://twitch.tv/Death Shop")
 client.user.setStatus("dnd")
 });
 
